6f1d766e1d7f839de2b75ee33b2385a908b02bb4
sNj <Sh1nJiTEITA.TWSOL@gmail.com>
2023-10-05 20:15:03 +0300

    0.001-EnviromentUpgrade: Cmake, glm, glfw, catch2

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿// BladeForge.cpp : Defines the entry point for the application.
+//
+
+#include "BladeForge.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
